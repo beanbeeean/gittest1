80f80447b194a2bcabf4dcd22c6487bebf46b959
beeean <beanbeeean@naver.com>
2021-11-09 17:58:30 +0900

    한글깨짐 확인

--- chapter06/src/ch05/Wrapper_Ex3.java
+++ chapter06/src/ch05/Wrapper_Ex3.java
@@ -3,48 +3,58 @@ package ch05;
 public class Wrapper_Ex3 {
 
 	public static void main(String[] args) {
-
+		
 		String str = "10";
 		String str2 = "10.5";
 		String str3 = "true";
-
-		// 8 + "" >>> String
+		
+		// 8 + "" --> String 
 		byte b = Byte.parseByte(str);
 		int i = Integer.parseInt(str);
 		short s = Short.parseShort(str);
-		long l = Long.parseLong(str); // ì•ˆë…•
-		//
+		long l = Long.parseLong(str);
+		
+		
+		
+		// 
 		float f = Float.parseFloat(str2);
-		Double d = Double.parseDouble(str2);
+		double d = Double.parseDouble(str2);
+		
 		boolean bool = Boolean.parseBoolean(str3);
 		System.out.println(bool);
-		if (Boolean.parseBoolean(str3)) {
-			System.out.println("boolean ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ï·ï¿½!!");
-		}
-
-		System.out.println("ï¿½ï¿½ï¿½Ú¿ï¿½ byte ï¿½ï¿½ ï¿½ï¿½È¯ : " + b);
-		System.out.println("ï¿½ï¿½ï¿½Ú¿ï¿½ int ï¿½ï¿½ ï¿½ï¿½È¯ : " + i);
-		System.out.println("ï¿½ï¿½ï¿½Ú¿ï¿½ short ï¿½ï¿½ ï¿½ï¿½È¯ : " + s);
-
-		System.out.println(b);
-		// ï¿½ï¿½ï¿½ï¿½ 1
-
-		String str10 = "1001";
-		String str11 = "2001";
-
-		// str10 ï¿½ï¿½ str11ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
-		// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
-		// str10 ï¿½ï¿½ str11ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
-		// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
-
-		int n = Integer.parseInt(str10);
-		int m = Integer.parseInt(str11);
-		int x = n + m;
-		int y = n * m;
-
-		System.out.println(x);
-		System.out.println(y);
-
-		// í•œê¸€ ê¹¨ì§ í™•ì¸ì½”ë“œ!
+//		if(Boolean.parseBoolean(str3)) {
+//			System.out.println("boolean ÇüÀ¸·Î º¯°æ ¿Ï·á!!!");
+//		}
+//		
+		// ¹®Á¦ 1 
+		
+//		String str10 = "1001";
+//		String str11 = "2001";
+		
+		// str10 °ú str11 À» µ¡¼À ¿¬»ê 
+		// °á°ú°ª ±îÁö Ãâ·Â 
+		// str10 °ú str11 À» °ö¼À ¿¬»ê
+		// °á°ú°ª ±îÁö Ãâ·Â 
+		
+		String str10 ="1001";
+		String str11 ="2001";
+		
+		int a = Integer.parseInt(str10);
+		int c = Integer.parseInt(str11);
+		int m = a + c;
+		int p = a * c;
+		
+		System.out.println("µÎ StringÀÇ ÇÕ °ªÀº : " + m + "ÀÔ´Ï´Ù");
+		System.out.println("µÎ StringÀÇ °ö °ªÀº : " + p + "ÀÔ´Ï´Ù");
+		
+//		System.out.println("¹®ÀÚ¿­ byte°ª º¯È¯ :" + b);
+//		System.out.println("¹®ÀÚ¿­ int°ª º¯È¯ : " + i);
+//		System.out.println(b);
+		
+		
+		// ÇÑ±Û±úÁü È®ÀÎ ÄÚµå !!!!!!
+		
+		
 	}
+
 }
